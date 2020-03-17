e16e0b5de6896be7f856a03a0dd7b7c784f2376a
unknown <854103093@qq.com>
2020-03-17 21:12:55 +0800

    获取轮播图的接口做了改动

---
+++ server/func/readFilelist.js
@@ -0,0 +1,23 @@
+
+const fs = require('fs');
+const path = require('path');
+
+function readFileList(dir, filesList = []) {
+    const files = fs.readdirSync(dir);
+    console.log(files);
+    files.forEach((item, index) => {
+        var fullPath = path.join(dir, item);
+        const stat = fs.statSync(fullPath);
+        if (stat.isDirectory()) {      
+            readFileList(path.join(dir, item), filesList);  //递归读取文件
+        } else {                
+            filesList.push(fullPath);                     
+        }        
+    });
+    return filesList;
+}
+
+module.exports = readFileList;
+ 
+// var filesList = [];
+// readFileList(__dirname,filesList);
--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+var url = require("url");
+const os = require("os");
 const app = express();
 // 在 app 文件夹开启静态服务
 app.use(express.static('../app'));
+
 
 //允许跨域
 var allowCrossDomain = function(req, res, next){
@@ -15,40 +19,52 @@ app.use(allowCrossDomain);
 var compression = require('compression');
 app.use(compression());
 
-app.get('/api/getlunbo',function(req, res){
-    res.send({
-            message: [
-              {
-                id: 1,
-                img: 'http://122.51.205.63:8090/imgResource/tiantian.jpg'
-                // img: 'http://localhost:8090/imgResource/tiantian.jpg'
-              },
-              {
-                id: 2,
-                img: 'http://122.51.205.63:8090/imgResource/minmin1.jpg'
-                // img: 'http://localhost:8090/imgResource/minmin1.jpg'
-              },
-              {
-                id: 3,
-                img: 'http://122.51.205.63:8090/imgResource/minmin2.jpg'
-                // img: 'http://localhost:8090/imgResource/minmin2.jpg'
-              }
-            ],
-            status: 0
-        }
-    );
-})
-
 //正则匹配 前端history模式路由
 const routerExp = /^\/home|search|cart|member/;
 app.get(routerExp, (req, res) => {
   //window系统
-  return res.sendFile(path.resolve('/html_node/node_server_project/app/index.html'));
+  // return res.sendFile(path.resolve('/html_node/node_server_project/app/index.html'));
   // linux系统
-  //return res.sendFile(path.resolve('../app/index.html'));
+  return res.sendFile(path.resolve('../app/index.html'));
 });
-// app.get(routerExp, (req, res) => res.sendFile('F:\html_node\node_server_project\app\index.html'));
+
+//读取图片
+//根据当前操作系统选择文件夹
+var resourceDir = '', rootURL = '';
+var platform = os.platform();
+console.log(platform);
+if(platform.match(/^[win]/)){
+  resourceDir = '/html_node/node_server_project/app';
+  rootURL = 'http://127.0.0.1:8090';
+}else if(platform == 'linux'){
+  resourceDir = '../app';
+  rootURL = 'http://122.51.205.63:8090'
+}else{
+  throw err;
+}
+var imgLunbotuDir = 'imgResource'; //图片文件夹
+function getImgUrl(rootdir, imgdir){
+  var dirpath = path.resolve(rootdir, imgdir);
+  var dirpath_parse = path.parse(dirpath);
+  const fileslist = fs.readdirSync(dirpath);
+  // console.log(fileslist);
+  return fileslist.map((file, i)=>({
+    id: i,
+    img: `${rootURL}/${imgdir}/${file}`
+  }))
+}
+
+app.get('/api/getlunbo',function(req, res){
+  console.log(req.connection.remoteAddress)
+  res.send({
+          message: getImgUrl(resourceDir, imgLunbotuDir),
+          status: 0
+      }
+  );
+})
+
+
 
 app.listen(8090, () => {
-  console.log('Demo server listening on port 8090');
+  console.log('Demo server listening on port 8090--');
 });
